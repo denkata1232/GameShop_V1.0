97681f349b375f73947695375c59cbe14a4ca30a
denkata1232 <angelovd357@gmail.com>
2025-04-13 10:29:38 +0300

    added business logic to: Product User TypeProduct

---
+++ Business/businessLogic/ProductBusiness.cs
@@ -0,0 +1,58 @@
+﻿using Data;
+using Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Business.businessLogic
+{
+    public class ProductBusiness
+    {
+        private GameShopContext context;
+
+        public ProductBusiness(GameShopContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return context.Products.ToList();
+        }
+
+        public Product GetProductById(int id)
+        {
+            return context.Products.Find(id);
+        }
+
+        public string AddProduct(Product product)
+        {
+            context.Products.Add(product);
+            context.SaveChanges();
+            return $"Product: {product.Name} added successfully";
+        }
+
+        public string UpdateProduct(Product product)
+        {
+            Product productToUpdate = context.Products.Find(product.ProductId);
+            if (productToUpdate != null)
+            {
+                context.Entry(productToUpdate).CurrentValues.SetValues(product);
+                context.SaveChanges();
+                return $"Product: {product.Name} updated successfully";
+            }
+            return $"Product with Name: {product.Name} not found";
+        }
+
+        public string DeleteProduct(Product product)
+        {
+            Product productToDelete = context.Products.Find(product.ProductId);
+            if (productToDelete != null)
+            {
+                context.Products.Remove(productToDelete);
+                context.SaveChanges();
+                return $"Product: {productToDelete.Name} deleted successfully";
+            }
+            return $"Product with Name: {product.Name} not found";
+        }
+    }
+}
---
+++ Business/businessLogic/UserBusiness.cs
@@ -0,0 +1,61 @@
+﻿using Data;
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.businessLogic
+{
+    public class UserBusiness
+    {
+        private GameShopContext context;
+
+        public UserBusiness(GameShopContext context)
+        {
+            this.context = context;
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return context.Users.ToList();
+        }
+
+        public User GetUserById(int id)
+        {
+            return context.Users.Find(id);
+        }
+
+        public string AddUser(User user)
+        {
+            context.Users.Add(user);
+            context.SaveChanges();
+            return $"User: {user.UserName} added successfully";
+        }
+
+        public string UpdateUser(User user)
+        {
+            User userToUpdate = context.Users.Find(user.UserId);
+            if (userToUpdate != null)
+            {
+                context.Entry(userToUpdate).CurrentValues.SetValues(user);
+                context.SaveChanges();
+                return $"User: {user.UserName} updated successfully";
+            }
+            return $"User with Name: {user.UserName} not found";
+        }
+
+        public string DeleteUser(User user)
+        {
+            User userToDelete = context.Users.Find(user.UserId);
+            if (userToDelete != null)
+            {
+                context.Users.Remove(userToDelete);
+                context.SaveChanges();
+                return $"User: {userToDelete.UserName} deleted successfully";
+            }
+            return $"User with Name: {user.UserName} not found";
+        }
+    }
+}
